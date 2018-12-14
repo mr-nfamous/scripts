cf63effd3e2b8725531c9f39c067f5546762d813
Dan Snider <mr.assume.away@gmail.com>
2018-12-13 23:27:40 -0600

    Update wrangler.py

--- trivialities/wrangler.py
+++ trivialities/wrangler.py
@@ -2,10 +2,9 @@ import re
 # I've probably spent *hours* trying to come up with a pattern that even works
 # to detect whether Python will mangle a name. Apparently, the key to understanding
 # negative lookbehind assertions is to stay awake for 27 hours...
-
+# However, I of course uploaded the wrong pattern on the first try.
 pat = re.compile('\A'
-                 '__'
-                 '[0-9A-Za-z]'
-                 '[0-9A-Za-z_]*'
+                 '___*'
+                 '[0-9A-Za-z_]+'
                  '(?<!__)'
                  '\Z')
