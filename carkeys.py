8b6488aee64644ab77ae4f85ea6dc352b57df5d0
Dan Snider <mr.assume.away@gmail.com>
2019-04-29 04:13:05 -0500

    Update carkeys.py

--- carkeys.py
+++ carkeys.py
@@ -2,13 +2,51 @@ def join(sep, seq):
 	seq = iter(seq)
 	one = next(seq, seq)
 	if one is seq: 
-      return seq
+        return seq
 	two = next(seq)
 	if two is seq: 
       return [one].__iter__
 	it = zip_longest(
-      [(one, sep)], 
-      zip(chain([two], seq)),
-      fillvalue=(sep,))
+        [(one, sep)], 
+        zip(chain([two], seq)),
+        fillvalue=(sep,))
 	it = starmap(concat, it)
 	return chained(it)
+    
+def startswith(seq, prefix, *subseq):
+    if not prefix:    return True
+    if seq == prefix: return True
+    if subseq:
+        if len(subseq) == 1:
+            [a] = subseq
+            seq = islice(seq, a, None)
+        else:
+            seq = islice(seq, *subseq)
+    for i, j in zip_longest(seq, prefix, fillvalue=nanny):
+        if i != j:    return j is nanny
+    return True
+
+def sortmap(mp, key=None, reverse=False):
+    try:
+        pop = mp.pop
+    except:
+        def pop(k):
+            v = mp[k]
+            del mp[k]
+            return v
+    for k in sorted(mp, key=key, reverse=reverse): mp[k] = pop(k)
+
+def summer(i, *j):
+    if j: lo, [hi] = i, j
+    else: lo,  hi  = 0, i
+    j = (hi-1) * hi//2
+    return (j-((lo-1)*lo//2)) if lo else j    if lo:
+        return ( - ((lo-1) * lo//2)
+    
+    if not j:
+        j = i
+        i = 0
+    
+        if not j:
+            return 0
+        return (j-1) * j//2
